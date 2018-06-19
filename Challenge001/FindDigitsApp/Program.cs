1ecbfb32055f6cdcada4b7a2fbe122c871e9a783
Armando Martinez <jmtzpena@gmail.com>
2018-06-19 00:48:48 -0500

    Add solution to challenge 001

---
+++ Challenge001/FindDigitsApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FindDigitsApp
+{
+    using System;
+    using Shouldly;
+    using FindDigits;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = ValidateArgs(args);
+            
+            foreach (var divisors in new FindDigit().FindDigits(numbers))
+            {
+                Console.WriteLine(divisors);
+            }
+        }
+
+        private static int[] ValidateArgs(string[] args)
+        {
+            args.Length.ShouldBeGreaterThan(0);
+
+            var numbers = new int[args.Length];
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                int number;
+
+                Int32.TryParse(args[i], out number).ShouldBe(true);
+                numbers[i] = number;
+            }
+
+            return numbers;
+        }
+    }
+}
